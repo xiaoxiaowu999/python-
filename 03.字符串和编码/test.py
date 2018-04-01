d3479efa936956487b703feed88df5914f75c175
wuxj <237129445@qq.com>
2018-04-01 12:01:54 +0800

    完成 字符串和编码 编码

--- 03.字符串和编码/test.py
+++ 03.字符串和编码/test.py
@@ -1,1 +1,19 @@
-print('hello word')
+
+print(ord('中'), ord('文'), chr(66), chr(25991),  '\u4e2d\u6587')
+
+
+# '中文'.encode('utf-8'), '1234556'.encode('ascii')
+print(b'\xe4\xb8\xad\xff'.decode('utf-8', errors='ignore'))
+
+
+print('%2d - %04d' % (3, 1))
+print('%s %.2f' % ('pi=', 3.1415))
+
+
+s1 = 72
+s2 = 85
+
+r = ((s2-s1)/s1) * 100
+
+print('小明成绩上升%.1f%%' % r)
+

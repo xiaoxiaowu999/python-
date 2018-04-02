15865b43ca38b1300558dda41d7adf0604cd806d
wuxj <237129445@qq.com>
2018-04-02 09:29:02 +0800

    提交 输入和输出 数据类型和变量 编程练习

---
+++ 01.输入和输出/test.py
@@ -0,0 +1,14 @@
+print('hello word')
+
+print('I', 'want to see', 'overlord')
+
+print(300+200,6+5)
+
+name = input('please input your name：')
+
+print('my name:', name)
+
+#test
+#请利用print()输出1024 * 768 = xxx：
+
+print('1024 * 768 =', 1024*768)
---
+++ 02.数据类型和变量/test.py
@@ -0,0 +1,47 @@
+#这是我的注释
+
+a = 1.235e8
+
+if a >=0 :
+    print(a)    
+else :
+    print(-a)
+
+print(r'''hello,\n
+world''')
+
+
+
+b1 = True
+b2 = False
+
+b3 = b1 and b2
+print(b3)
+b3 = b1 or b2
+print(b3)
+b3 = not b2
+print(b3)
+
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+
+print('测试除法1\n', 10/3)
+print('测试除法2\n', 10//3)
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,\n
+Lisa!'''
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
